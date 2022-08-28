09e7d89d1d9a06c5ac148edafa1a4ca1c2dbe1e0
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2022-08-28 10:30:42 +0700

    fix button

--- components/button.js
+++ components/button.js
@@ -6,38 +6,38 @@ function variantStyles({ variant, color }) {
   switch (variant) {
     case 'filled':
       return {
-        'bg-sky-500 text-white enabled:hover:bg-sky-700 focus:ring focus:ring-sky-300':
+        'bg-sky-500 text-white enabled:hover:bg-sky-600 enabled:active:bg-sky-700 focus:ring focus:ring-sky-300':
           color === 'sky',
-        'bg-red-500 text-white enabled:hover:bg-red-700 focus:ring focus:ring-red-300':
+        'bg-red-500 text-white enabled:hover:bg-red-600 enabled:active:bg-red-700 focus:ring focus:ring-red-300':
           color === 'red',
-        'bg-green-500 text-white enabled:hover:bg-green-700 focus:ring focus:ring-green-300':
+        'bg-green-500 text-white enabled:hover:bg-green-600 enabled:active:bg-green-700 focus:ring focus:ring-green-300':
           color === 'green',
       };
     case 'light':
       return {
-        'bg-sky-50 text-white enabled:hover:bg-sky-100 focus:ring-1 focus:ring-sky-300 text-sky-600':
+        'bg-sky-50 text-white enabled:hover:bg-sky-100 enabled:active:bg-sky-200 focus:ring-2 focus:ring-sky-300 text-sky-600':
           color === 'sky',
-        'bg-red-50 text-white enabled:hover:bg-red-100 focus:ring-1 focus:ring-red-300 text-red-600':
+        'bg-red-50 text-white enabled:hover:bg-red-100 enabled:active:bg-red-200  focus:ring-2 focus:ring-red-300 text-red-600':
           color === 'red',
-        'bg-green-50 text-white enabled:hover:bg-green-100 focus:ring-1 focus:ring-green-300 text-green-600':
+        'bg-green-50 text-white enabled:hover:bg-green-100 enabled:active:bg-green-200  focus:ring-2 focus:ring-green-300 text-green-600':
           color === 'green',
       };
     case 'outline':
       return {
-        'text-white enabled:hover:bg-sky-50 focus:ring-1 focus:ring-sky-200 text-sky-600 border border-sky-600':
+        'text-white enabled:hover:bg-sky-50 enabled:active:bg-sky-100 focus:ring-2 focus:ring-sky-200 text-sky-600 border border-sky-600':
           color === 'sky',
-        'text-white enabled:hover:bg-red-50 focus:ring-1 focus:ring-red-200 text-red-600 border border-red-600':
+        'text-white enabled:hover:bg-red-50 enabled:active:bg-red-100 focus:ring-2 focus:ring-red-200 text-red-600 border border-red-600':
           color === 'red',
-        'text-white enabled:hover:bg-green-50 focus:ring-1 focus:ring-green-200 text-green-600 border border-green-600':
+        'text-white enabled:hover:bg-green-50 enabled:active:bg-green-100 focus:ring-2 focus:ring-green-200 text-green-600 border border-green-600':
           color === 'green',
       };
     case 'subtle':
       return {
-        'text-white enabled:hover:bg-sky-50 focus:ring-1 focus:ring-sky-200 text-sky-600':
+        'text-white enabled:hover:bg-sky-50 enabled:active:bg-sky-100  focus:ring-2 focus:ring-sky-200 text-sky-600':
           color === 'sky',
-        'text-white enabled:hover:bg-red-50 focus:ring-1 focus:ring-red-200 text-red-600':
+        'text-white enabled:hover:bg-red-50 enabled:active:bg-red-100 focus:ring-2 focus:ring-red-200 text-red-600':
           color === 'red',
-        'text-white enabled:hover:bg-green-50 focus:ring-1 focus:ring-green-200 text-green-600':
+        'text-white enabled:hover:bg-green-50 enabled:active:bg-greeb-100 focus:ring-2 focus:ring-green-200 text-green-600':
           color === 'green',
       };
     default:
@@ -82,7 +82,7 @@ function Button({
   ...props
 }) {
   let style = clsx(
-    `transition-all duration-200 ease-out focus:outline-none font-semibold`,
+    `transition-all focus:outline-none font-semibold`,
     variantStyles({ variant, color }),
     roundedStyles(rounded),
     sizeStyles(size),
