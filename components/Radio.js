6725daff3e6c6640d9521175b5bdd2452cab60ee
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2022-12-14 14:47:46 +0700

    add radio

--- components/Radio.js
+++ components/Radio.js
@@ -1,63 +1,74 @@
 import cx from "clsx";
-import React from "react";
+import React, { useId } from "react";
 import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
 
-const starters = [
-  { id: "red", title: "Bulbasaur" },
-  { id: "green", title: "Charmader" },
-  { id: "blue", title: "Squirtle" },
-];
-
-const RadioGroup = ({ color = "blue" }) => {
-  const [value, setValue] = React.useState(starters[0].title);
-
+export function Item({ value, label, size = "md", color = "purple" }) {
+  let id = useId();
   return (
-    <form>
-      <legend className="text-sm font-medium leading-4 text-gray-900 dark:text-gray-100">
-        Choose your starter
-      </legend>
-      <RadioGroupPrimitive.Root
-        aria-label="Pokemon starters"
-        defaultValue={"Bulbasaur"}
-        onValueChange={setValue}
+    <div className="flex items-center space-x-3">
+      <RadioGroupPrimitive.Item
+        id={id}
+        value={value}
+        className={cx(
+          "relative rounded-full",
+          "data-[state=unchecked]:border data-[state=unchecked]:border-gray-300",
+          "focus:outline-none focus:ring-0 focus:ring-offset-0",
+          "focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-offset-1 ",
+          {
+            "h-4 w-4": size === "sm",
+            "h-5 w-5": size === "md",
+            "h-6 w-6": size === "lg",
+            "h-7 w-7": size === "xl",
+          },
+          {
+            "focus-visible:ring-purple-500 data-[state=checked]:bg-purple-500":
+              color === "purple",
+            "focus-visible:ring-sky-500 data-[state=checked]:bg-sky-500":
+              color === "sky",
+            "focus-visible:ring-yellow-500 data-[state=checked]:bg-yellow-500":
+              color === "yellow",
+          },
+        )}
       >
-        <div className="mt-3 space-y-3">
-          {starters.map((pokemon) => (
-            <div key={pokemon.id} className="flex items-center">
-              <RadioGroupPrimitive.Item
-                id={pokemon.id}
-                value={pokemon.title}
-                className={cx(
-                  "relative h-5 w-5 rounded-full",
-                  "data-[state=unchecked]:border data-[state=unchecked]:border-gray-300",
-                  "focus:outline-none focus:ring-0 focus:ring-offset-0",
-                  "focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-offset-1 ",
-                  {
-                    "focus-visible:ring-purple-500 data-[state=checked]:bg-purple-500":
-                      color === "purple",
-                    "focus-visible:ring-sky-500 data-[state=checked]:bg-sky-500":
-                      color === "blue",
-                    "focus-visible:ring-yellow-500 data-[state=checked]:bg-yellow-500":
-                      color === "yellow",
-                  },
-                )}
-              >
-                <RadioGroupPrimitive.Indicator className="leading-0 absolute inset-0 flex items-center justify-center">
-                  <div className="h-1.5 w-1.5 rounded-full bg-white"></div>
-                </RadioGroupPrimitive.Indicator>
-              </RadioGroupPrimitive.Item>
-              <label
-                htmlFor={pokemon.id}
-                className="ml-2 block text-sm font-medium text-gray-700 "
-              >
-                {pokemon.title}
-              </label>
-            </div>
-          ))}
-        </div>
-      </RadioGroupPrimitive.Root>
-    </form>
+        <RadioGroupPrimitive.Indicator
+          className={cx(
+            "flex items-center justify-center",
+            "leading-0 absolute inset-0",
+          )}
+        >
+          <div
+            className={cx("rounded-full bg-white", {
+              "h-1.5 w-1.5": size === "sm",
+              "h-2 w-2": size === "md",
+              "h-2.5 w-2.5": size === "lg",
+              "h-3 w-3": size === "xl",
+            })}
+          ></div>
+        </RadioGroupPrimitive.Indicator>
+      </RadioGroupPrimitive.Item>
+      <label
+        htmlFor={id}
+        className={cx("block text-base text-gray-700", {
+          "text-sm": size === "sm",
+          "text-base": size === "md",
+          "text-lg": size === "lg",
+          "text-xl": size === "xl",
+        })}
+      >
+        {label}
+      </label>
+    </div>
   );
-};
+}
 
-export default RadioGroup;
+export function Group({ value, onChange, className, children }) {
+  return (
+    <RadioGroupPrimitive.Root
+      className={className}
+      value={value}
+      onValueChange={onChange}
+    >
+      {children}
+    </RadioGroupPrimitive.Root>
+  );
+}
--- pages/radio.js
+++ pages/radio.js
@@ -1,10 +1,30 @@
 import React from "react";
-import Radio from "../components/Radio";
+import * as Radio from "../components/Radio";
 function Checkbox() {
-  // let [checked, setChecked] = useState("indeterminate");
+  const [value, setValue] = React.useState("3");
   return (
-    <div className="p-10">
-      <Radio></Radio>
+    <div className="p-10 space-y-5">
+      <Radio.Group className="flex space-x-5" value={value} onChange={setValue}>
+        <Radio.Item size="md" color="sky" label="option 1" value="1"></Radio.Item>
+        <Radio.Item size="md" color="sky" label="option 2" value="2"></Radio.Item>
+        <Radio.Item size="md" color="sky" label="option 3" value="3"></Radio.Item>
+      </Radio.Group>
+      <Radio.Group className="flex space-x-5" value={value} onChange={setValue}>
+        <Radio.Item size="md" color="sky" label="option 1" value="1"></Radio.Item>
+        <Radio.Item size="md" color="sky" label="option 2" value="2"></Radio.Item>
+        <Radio.Item size="md" color="sky" label="option 3" value="3"></Radio.Item>
+      </Radio.Group>
+      <Radio.Group className="flex space-x-5" value={value} onChange={setValue}>
+        <Radio.Item size="md" color="sky" label="option 1" value="1"></Radio.Item>
+        <Radio.Item size="md" color="sky" label="option 2" value="2"></Radio.Item>
+        <Radio.Item size="md" color="sky" label="option 3" value="3"></Radio.Item>
+      </Radio.Group>
+      <Radio.Group className="flex space-x-5" value={value} onChange={setValue}>
+        <Radio.Item size="md" color="sky" label="option 1" value="1"></Radio.Item>
+        <Radio.Item size="md" color="sky" label="option 2" value="2"></Radio.Item>
+        <Radio.Item size="md" color="sky" label="option 3" value="3"></Radio.Item>
+      </Radio.Group>
+      
     </div>
   );
 }
