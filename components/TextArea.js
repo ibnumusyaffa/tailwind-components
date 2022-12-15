f7b16962e66180712f4f174cf6590264c700232d
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2022-12-15 20:55:16 +0700

    text area

--- components/TextArea.js
+++ components/TextArea.js
@@ -3,16 +3,12 @@ import clsx from "clsx";
 
 function TextArea({
   error,
-  disabled,
-  leftOutside,
-  leftIcon,
-  rightOutside,
-  rightIcon,
+  disabled = true,
   size = "md",
   color = "sky",
-  rounded = "sm",
+  rounded = "md",
   variant = "filled",
-  rows = "3",
+  rows = "4",
   ...props
 }) {
   return (
@@ -21,19 +17,22 @@ function TextArea({
       rows={rows}
       className={clsx(
         // base style
-        "z-10 w-full flex-1 px-3 py-2 outline-none transition-all duration-200 ease-in-out",
-        // variant style
-        {
-          "border-0 bg-gray-100 focus:bg-white": variant === "filled",
-          "bg-white-100 border border-gray-300": variant === "outline",
-          "!border border-red-500 ": error,
 
+        "z-10 w-full flex-1 text-gray-800 outline-none transition-all duration-200 ease-in-out",
+
+        {
+          "border-0 bg-gray-100 focus:bg-white focus:ring-2":
+            variant === "filled",
+          "bg-white-100 border border-gray-300 focus:ring-1":
+            variant === "outline",
+          "!border border-red-500 ": error,
           "cursor-not-allowed bg-gray-100 opacity-75": disabled,
         },
+
         //color style
         {
-          "focus:border-blue-500  focus:ring-blue-500": color === "blue",
-          "focus:border-sky-500  focus:ring-sky-500": color === "sky",
+          "focus:ring-blue-500 ": color === "blue",
+          "focus:ring-sky-500 ": color === "sky",
         },
         //size style
         {
--- pages/textarea.js
+++ pages/textarea.js
@@ -1,70 +1,69 @@
-import React from 'react';
-import TextArea from '../components/TextArea';
-import { ArrowLongRightIcon } from '@heroicons/react/24/solid';
+import React from "react";
+import TextArea from "../components/TextArea";
 function Page() {
   return (
     <div>
-      <div className="p-5 space-y-5 w-1/3">
+      <div className="w-1/3 space-y-5 p-5">
         <TextArea
           variant="outline"
           size="xs"
-          placeholder="Test"
+          placeholder="xs"
           type="text"
         ></TextArea>
         <TextArea
           variant="outline"
           size="sm"
-          placeholder="Test"
+          placeholder="sm"
           type="text"
         ></TextArea>
         <TextArea
           variant="outline"
           size="md"
-          placeholder="Testt"
+          placeholder="md"
           type="text"
         ></TextArea>
         <TextArea
           variant="outline"
           size="lg"
-          placeholder="Test"
+          placeholder="lg"
           type="text"
         ></TextArea>
         <TextArea
           variant="outline"
           size="xl"
-          placeholder="Test"
+          placeholder="xl"
           type="text"
         ></TextArea>
       </div>
-      <div className="p-5 space-y-5 w-1/3">
+      <div className="w-1/3 space-y-5 p-5">
         <TextArea
           variant="filled"
           size="xs"
-          placeholder="Test"
+          placeholder="xs"
           type="text"
         ></TextArea>
         <TextArea
           variant="filled"
           size="sm"
-          placeholder="Test"
+          placeholder="sm"
           type="text"
         ></TextArea>
         <TextArea
           variant="filled"
           size="md"
-          placeholder="Testt"
+          placeholder="md"
           type="text"
         ></TextArea>
         <TextArea
           variant="filled"
           size="lg"
-          placeholder="Test"
+          placeholder="lg"
           type="text"
         ></TextArea>
         <TextArea
           variant="filled"
           size="xl"
-          placeholder="Test"
+          placeholder="xl"
           type="text"
         ></TextArea>
       </div>
