b2dba06e334154c4b6cac573e394072bcfb8f3e2
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2022-12-15 20:29:07 +0700

    fix badge

--- components/Badge.js
+++ components/Badge.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from "react";
+import clsx from "clsx";
 
 function variantStyles({ variant, color }) {
   switch (variant) {
-    case 'filled':
+    case "solid":
       return {
-        'bg-sky-500 text-white': color === 'sky',
-        'bg-red-500 text-white': color === 'red',
-        'bg-green-500 text-white': color === 'green',
+        "bg-blue-500 text-white": color === "blue",
+        "bg-red-500 text-white": color === "red",
+        "bg-green-500 text-white": color === "green",
+        "bg-yellow-500 text-white": color === "yellow",
+        "bg-pink-500 text-white": color === "pink",
+        "bg-gray-500 text-white": color === "gray",
+        "bg-purple-500 text-white": color === "purple",
       };
-    case 'light':
+    case "light":
       return {
-        'bg-sky-50 text-sky-600': color === 'sky',
-        'bg-red-50 text-red-600': color === 'red',
-        'bg-green-50 ext-green-600': color === 'green',
+        "bg-blue-100 text-blue-700": color === "blue",
+        "bg-red-100 text-red-700": color === "red",
+        "bg-green-100 text-green-700": color === "green",
+        "bg-yellow-100 text-yellow-700": color === "yellow",
+        "bg-pink-100 text-pink-700": color === "pink",
+        "bg-gray-100 text-gray-700": color === "gray",
+        "bg-purple-100 text-purple-700": color === "purple",
       };
-    case 'outline':
+    case "outline":
       return {
-        'text-sky-600 border border-sky-600': color === 'sky',
-        'text-red-600 border border-red-600': color === 'red',
-        'text-green-600 border border-green-600': color === 'green',
+        "text-blue-600 border border-blue-600": color === "blue",
+        "text-red-600 border border-red-600": color === "red",
+        "text-green-600 border border-green-600": color === "green",
+
+        "text-yellow-600 border border-yellow-600": color === "yellow",
+        "text-pink-600 border border-pink-600": color === "pink",
+        "text-gray-600 border border-gray-600": color === "gray",
+        "text-purple-600 border border-purple-600": color === "purple",
+      };
+    default:
+      return null;
+  }
+}
+
+function dotStyles({ variant, color }) {
+  switch (variant) {
+    case "solid":
+      return {
+        "bg-white": true,
+      };
+    case "light":
+    case "outline":
+      return {
+        "bg-blue-500": color === "blue",
+        "bg-red-500": color === "red",
+        "bg-green-500": color === "green",
+        "bg-yellow-500": color === "yellow",
+        "bg-pink-500": color === "pink",
+        "bg-gray-500": color === "gray",
+        "bg-purple-500": color === "purple",
       };
     default:
       return null;
@@ -27,68 +62,51 @@ function variantStyles({ variant, color }) {
 }
 
 function Badge({
-  size = 'sm',
-  variant = 'filled',
-  color = 'sky',
-  rounded = 'none',
+  size = "sm",
+  variant = "filled",
+  color = "blue",
+  rounded = "sm",
   children,
   fullWidth = false,
-  leftIcon,
-  rightIcon,
+  withDot = true,
   ...props
 }) {
   let style = clsx(
-    `transition-all duration-200 ease-out focus:outline-none font-semibold px-3 flex items-center justify-center space-x-1.5`,
+    `transition-all duration-200 ease-out focus:outline-none font-semibold px-1.5 flex items-center justify-center space-x-1.5`,
     variantStyles({ variant, color }),
     {
-      'rounded-none': rounded === 'none',
-      'rounded': rounded === 'sm',
-      'rounded-md': rounded === 'md',
-      'rounded-lg': rounded === 'lg',
-      'rounded-xl': rounded === 'xl',
-      'rounded-full': rounded === 'full',
+      "rounded-none": rounded === "none",
+      "rounded": rounded === "sm",
+      "rounded-md": rounded === "md",
+      "rounded-lg": rounded === "lg",
+      "rounded-xl": rounded === "xl",
+      "rounded-full": rounded === "full",
     },
     {
-      'h-5 text-[11px]': size === 'xs',
-      'h-6 text-[12px]': size === 'sm',
-      'h-7 text-[13px]': size === 'md',
-      'h-8 text-[14px]': size === 'lg',
-      'h-10 text-[16px]': size === 'xl',
+      "h-[18px] text-[11px]": size === "xs",
+      "h-[20px] text-[12px]": size === "sm",
+      "h-[24px] text-[13px]": size === "md",
+      "h-[28px] text-[14px]": size === "lg",
+      "h-[34px] text-[15px]": size === "xl",
     },
     {
-      'w-full': fullWidth,
+      "w-full": fullWidth,
     },
   );
 
   return (
-    <span className={style} {...props}>
-      {leftIcon ? (
+    <span cl className={style} {...props}>
+      {withDot ? (
         <div
-          className={clsx({
-            'h-[13px] w-[13px]': size === 'xs',
-            'h-[14px] w-[14px]': size === 'sm',
-            'h-[15px] w-[15px]': size === 'md',
-            'h-[17px] w-[17px]': size === 'lg',
-            'h-[19px] w-[19px]': size === 'xl',
-          })}
-        >
-          {leftIcon}
-        </div>
+          className={clsx(
+            "rounded-full",
+            "h-1.5 w-1.5",
+            dotStyles({ variant, color }),
+          )}
+        ></div>
       ) : null}
+
       <div>{children}</div>
-      {rightIcon ? (
-        <div
-          className={clsx({
-            'h-[13px] w-[13px]': size === 'xs',
-            'h-[14px] w-[14px]': size === 'sm',
-            'h-[15px] w-[15px]': size === 'md',
-            'h-[17px] w-[17px]': size === 'lg',
-            'h-[19px] w-[19px]': size === 'xl',
-          })}
-        >
-          {rightIcon}
-        </div>
-      ) : null}
     </span>
   );
 }
--- pages/badge.js
+++ pages/badge.js
@@ -1,152 +1,114 @@
-import React from 'react';
-import Badge from '../components/Badge';
-import { ClockIcon, PlusIcon } from '@heroicons/react/24/outline';
+import React from "react";
+import Badge from "../components/Badge";
 function Page() {
   // let colors = ['sky', 'red', 'green'];
   return (
-    <div className="p-5 space-y-5">
+    <div className="space-y-5 p-5">
       <div className="flex space-x-5">
-        <Badge color="red" size="xs" variant="light">
-          Settings
+        <Badge color="red" variant="light">
+          Badge
         </Badge>
-        <Badge color="red" size="sm" variant="light">
-          Settings
+        <Badge color="green" variant="light">
+          Badge
         </Badge>
-        <Badge color="red" size="md" variant="light">
-          Settings
+        <Badge color="blue" variant="light">
+          Badge
         </Badge>
-        <Badge color="red" size="lg" variant="light">
-          Settings
+        <Badge color="yellow" variant="light">
+          Badge
         </Badge>
-        <Badge color="red" size="xl" variant="light">
-          Settings
+        <Badge color="pink" variant="light">
+          Badge
+        </Badge>
+        <Badge color="gray" variant="light">
+          Badge
+        </Badge>
+        <Badge color="purple" variant="light">
+          Badge
         </Badge>
       </div>
       <div className="flex space-x-5">
-        <Badge
-          leftIcon={<ClockIcon></ClockIcon>}
-          color="red"
-          size="xs"
-          variant="outline"
-        >
-          Settings
+        <Badge color="red" variant="solid">
+          Badge
         </Badge>
-        <Badge
-          leftIcon={<ClockIcon></ClockIcon>}
-          color="red"
-          size="sm"
-          variant="outline"
-        >
-          Settings
+        <Badge color="green" variant="solid">
+          Badge
         </Badge>
-        <Badge
-          leftIcon={<ClockIcon></ClockIcon>}
-          color="red"
-          size="md"
-          variant="outline"
-        >
-          Settings
+        <Badge color="blue" variant="solid">
+          Badge
         </Badge>
-        <Badge
-          leftIcon={<ClockIcon></ClockIcon>}
-          color="red"
-          size="lg"
-          variant="outline"
-        >
-          Settings
+        <Badge color="yellow" variant="solid">
+          Badge
         </Badge>
-        <Badge
-          leftIcon={<ClockIcon></ClockIcon>}
-          color="red"
-          size="xl"
-          variant="outline"
-        >
-          Settings
+        <Badge color="pink" variant="solid">
+          Badge
+        </Badge>
+        <Badge color="gray" variant="solid">
+          Badge
+        </Badge>
+        <Badge color="purple" variant="solid">
+          Badge
         </Badge>
       </div>
       <div className="flex space-x-5">
-        <Badge
-          leftIcon={<PlusIcon></PlusIcon>}
-          color="red"
-          size="xs"
-          variant="outline"
-        >
-          Settings
+        <Badge color="red" variant="outline">
+          Badge
         </Badge>
-        <Badge
-          leftIcon={<PlusIcon></PlusIcon>}
-          color="red"
-          size="sm"
-          variant="outline"
-        >
-          Settings
+        <Badge color="green" variant="outline">
+          Badge
         </Badge>
-        <Badge
-          leftIcon={<PlusIcon></PlusIcon>}
-          color="red"
-          size="md"
-          variant="outline"
-        >
-          Settings
+        <Badge color="blue" variant="outline">
+          Badge
         </Badge>
-        <Badge
-          leftIcon={<PlusIcon></PlusIcon>}
-          color="red"
-          size="lg"
-          variant="outline"
-        >
-          Settings
+        <Badge color="yellow" variant="outline">
+          Badge
         </Badge>
-        <Badge
-          leftIcon={<PlusIcon></PlusIcon>}
-          color="red"
-          size="xl"
-          variant="outline"
-        >
-          Settings
+        <Badge color="pink" variant="outline">
+          Badge
+        </Badge>
+        <Badge color="gray" variant="outline">
+          Badge
+        </Badge>
+        <Badge color="purple" variant="outline">
+          Badge
+        </Badge>
+      </div>
+
+      <div className="flex space-x-5">
+        <Badge size="xs" variant="light">
+          Badge xs
+        </Badge>
+        <Badge size="sm" variant="light">
+          Badge sm
+        </Badge>
+        <Badge size="md" variant="light">
+          Badge md
+        </Badge>
+        <Badge size="lg" variant="light">
+          Badge lg
+        </Badge>
+        <Badge size="xl" variant="light">
+          Badge xl
         </Badge>
       </div>
       <div className="flex space-x-5">
-        <Badge
-          leftIcon={<ClockIcon></ClockIcon>}
-          color="red"
-          size="xs"
-          variant="outline"
-          rounded='full'
-        >
-          Settings
+        <Badge rounded="none" variant="outline">
+          rounded none
         </Badge>
-        <Badge color="red" size="sm" variant="filled">
-          Settings
+        <Badge rounded="sm" variant="outline">
+          rounded sm
         </Badge>
-        <Badge color="red" size="md" variant="filled">
-          Settings
+        <Badge rounded="md" variant="outline">
+          rounded md
         </Badge>
-        <Badge color="red" size="lg" variant="filled">
-          Settings
+        <Badge rounded="lg" variant="outline">
+          rounded lg
         </Badge>
-        <Badge color="red" size="xl" variant="filled">
-          Settings
+        <Badge rounded="xl" variant="outline">
+          rounded xl
         </Badge>
-      </div>
-      <div className="flex space-x-5">
-        <Badge color="red" rounded="none" variant="outline">
-          Settings
-        </Badge>
-        <Badge color="red" rounded="sm" variant="outline">
-          Settings
-        </Badge>
-        <Badge color="red" rounded="md" variant="outline">
-          Settings
-        </Badge>
-        <Badge color="red" rounded="lg" variant="outline">
-          Settings
-        </Badge>
-        <Badge color="red" rounded="xl" variant="outline">
-          Settings
-        </Badge>
-        <Badge color="red" rounded="full" variant="outline">
-          Settings
+        <Badge rounded="full" variant="outline">
+          rounded full
         </Badge>
       </div>
     </div>
